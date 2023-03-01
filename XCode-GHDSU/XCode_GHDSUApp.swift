5211613092d75d4baf611e4dab5d5ede7eb3a88b
mckdscaufield <daniel_caufield@mckinsey.com>
2023-03-01 17:47:16 +0100

    Initial Commit

---
+++ XCode-GHDSU/XCode_GHDSUApp.swift
@@ -0,0 +1,17 @@
+//
+//  XCode_GHDSUApp.swift
+//  XCode-GHDSU
+//
+//  Created by Daniel Caufield on 01.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct XCode_GHDSUApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
